1b2ce69a86a56d712d1f820fab97bc426f40521f
root <root@aceslab.umd.edu>
2016-05-04 00:00:04 -0400

    Wed May  4 00:00:04 EDT 2016

--- Old/shit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while [ 1 ]
-do 
-echo "-------------------------`date`-------------------------" >> crap
-ps aux --sort -rss | head -10 >> crap
-echo "=========================Honeypots=========================" >> crap
-sudo vzmemcheck -v | sed 's/\s\s*/ /g' | cut -d ' ' -f 1,4 | tail -5 >> crap
-sleep 5
-done
